b2d05fca25cafbc0eadc0d3913625f2a616eb4c5
Eugene Ng <eugenen@utexas.edu>
2025-07-28 02:11:01 -0700

    refactor; address pylint

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # srs-with-ctg-eff-diff
+
 Implement the Simple Rating System algorithm and change its input from raw point differential to CleaningTheGlass's Efficiency Differential
 
 Links describing the concepts used:
 
 https://web.archive.org/web/20161031224357/http://www.pro-football-reference.com/blog/index4837.html
-* https://www.basketball-reference.com/blog/indexba52.html?p=39
-* https://www.sports-reference.com/blog/2015/03/srs-calculation-details/
+
+- https://www.basketball-reference.com/blog/indexba52.html?p=39
+- https://www.sports-reference.com/blog/2015/03/srs-calculation-details/
 
 https://cleaningtheglass.com/stats/league/fourfactors
-* https://cleaningtheglass.com/stats/guide/league_four_factors
-* https://cleaningtheglass.com/stats/guide/garbage_time
+
+- https://cleaningtheglass.com/stats/guide/league_four_factors
+- https://cleaningtheglass.com/stats/guide/garbage_time
+
+Todo:
+
+- use API calls to retrieve league / season / team / game data instead of relying on files for input
+- organize code into classes or namespaces instead of having everything in one script
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from numpy.linalg import lstsq # use scipy or numpy?
-from numpy import dot
 
 @dataclass
 class TeamInfo:
@@ -8,14 +7,20 @@ class TeamInfo:
     pt_diff = 0
     ctg_eff_diff = 0
     gp_vs_teams: list
-    
+
     def num_games(self) -> int:
         # WARNING: do not use this method before indices has been defined
         return -self.gp_vs_teams[indices[self.name]]
 
-with open('game_log.txt', 'r') as results_file:
-    # should we get a list of teams from API or lazily build this up as we parse game results? or iterate thru results twice to first gather teams into list?
-    # probably draw from API for efficiency and intuitiveness even though it kind of creates 2 sources of truth
+LINALG_ERROR_MSG = '\n'.join((
+    "Failed to properly solve linear system:",
+    "Expected residuals to be [], got {}",
+    "Expected rank to be {}, got {}"
+))
+
+with open('game_log.csv', 'r', encoding="utf-8") as results_file:
+    # how to get list of teams: from API or an extra preliminary iteration thru game results?
+    # API probably makes more sense assuming it's consistent with the team names it uses
     teams = set()
     for result in results_file:
         teams |= set(result.split(',')[2:5:2])
@@ -29,28 +34,29 @@ with open('game_log.txt', 'r') as results_file:
         visitor, visitor_score, home, home_score = (int(x) if x.isdigit() else x for x in result.split(',')[2:6])
         v_ind, h_ind = indices[visitor], indices[home]
         v_teaminfo, h_teaminfo = teaminfos[v_ind], teaminfos[h_ind]
-        
+
         v_teaminfo.pt_diff += visitor_score - home_score
         h_teaminfo.pt_diff += home_score - visitor_score
-        
+
         v_teaminfo.gp_vs_teams[h_ind] += 1
         v_teaminfo.gp_vs_teams[v_ind] -= 1
         h_teaminfo.gp_vs_teams[v_ind] += 1
         h_teaminfo.gp_vs_teams[h_ind] -= 1
-    
+
 negated_pt_diffs = [-teaminfo.pt_diff for teaminfo in teaminfos]
 try:
     srs_vals = lstsq(sched, negated_pt_diffs)
     if srs_vals[1].size > 0 or srs_vals[2] != NUM_TEAMS - 1:
-        raise ValueError(f"Failed to properly solve linear system:\nExpected residuals to be [], got {srs_vals[1]}\nExpected rank to be {NUM_TEAMS - 1}, got {srs_vals[2]}")
+        raise ValueError(LINALG_ERROR_MSG.format(srs_vals[1], NUM_TEAMS - 1, srs_vals[2]))
 except ValueError as e:
+    # maybe in the future do more than just print the msg
     print(e)
 
 for teaminfo, srs in sorted(zip(teaminfos, srs_vals[0]), key=lambda x: x[1], reverse=True):
     print(teaminfo.name, f"{teaminfo.pt_diff / teaminfo.num_games():.2f}", f"{srs:.2f}")
 print()
 
-with open('league_four_factors_7_27_2025.csv', 'r') as ctg_file:
+with open('league_four_factors_7_27_2025.csv', 'r', encoding="utf-8") as ctg_file:
     for team_stats in ctg_file:
         team, diff = team_stats.split(',')[0:5:4]
         if team == 'Team' or team == 'Average':
@@ -58,22 +64,24 @@ with open('league_four_factors_7_27_2025.csv', 'r') as ctg_file:
         try:
             found = False
             for teaminfo in teaminfos:
-                # NOTE: don't know best way to consolidate the different ways different sources name teams
+                # NOTE: Will need to update name determination when using API
                 if team.split()[-1] in teaminfo.name:
                     found = True
                     teaminfo.ctg_eff_diff = float(diff)
                     break
-            if found == False:
+            if not found:
                 raise ValueError(f"Could not figure out which team matches {team}.")
         except ValueError as e:
+            # maybe in the future do more than just print the msg
             print(e)
 
 negated_ctg = [-teaminfo.ctg_eff_diff * teaminfo.num_games() for teaminfo in teaminfos]
 try:
     srs_ctg_vals = lstsq(sched, negated_ctg)
     if srs_ctg_vals[1].size > 0 or srs_ctg_vals[2] != NUM_TEAMS - 1:
-        raise ValueError(f"Failed to properly solve linear system:\nExpected residuals to be [], got {srs_ctg_vals[1]}\nExpected rank to be {NUM_TEAMS - 1}, got {srs_ctg_vals[2]}")
+        raise ValueError(LINALG_ERROR_MSG.format(srs_ctg_vals[1], NUM_TEAMS - 1, srs_ctg_vals[2]))
 except ValueError as e:
+    # maybe in the future do more than just print the msg
     print(e)
 
 for teaminfo, srs in sorted(zip(teaminfos, srs_ctg_vals[0]), key=lambda x: x[1], reverse=True):
